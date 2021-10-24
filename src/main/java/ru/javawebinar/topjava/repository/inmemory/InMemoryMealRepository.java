c67b05ed7a10c059e753e495a3d28d3ddf4c5249
Vitalik Zhukau <unicraft1989@gmail.com>
2021-10-24 19:35:06 +0300

    Implement class MealService.

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -5,12 +5,9 @@ import org.slf4j.LoggerFactory;
 
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
-import ru.javawebinar.topjava.util.MealsUtil;
 
-import java.time.LocalDateTime;
-import java.time.Month;
-import java.util.Collection;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -25,7 +22,6 @@ public class InMemoryMealRepository implements MealRepository {
     @Override
     public Meal save(int userID, Meal meal) {
         log.info("save {} {}", userID, meal);
-        if (userID != meal.getId()) return null;
         //save
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
@@ -40,19 +36,17 @@ public class InMemoryMealRepository implements MealRepository {
     @Override
     public boolean delete(int userID, int id) {
         log.info("delete {} {}", userID, id);
-        if(userID != id) return false;
         return getUserIdMealRepository(userID).remove(id) != null;
     }
 
     @Override
     public Meal get(int userID, int id) {
         log.info("get {} {}", userID, id);
-        if (userID != id) return null;
         return getUserIdMealRepository(userID).get(id);
     }
 
     @Override
-    public Collection<Meal> getAll(int userID) {
+    public List<Meal> getAll(int userID) {
         log.info("getAll");
         return getUserIdMealRepository(userID).values().stream()
                 .sorted(Comparator.comparing(Meal::getDate).reversed())
